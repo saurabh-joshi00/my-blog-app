d6d49fc17a101570050011fba6d8ccf4d6fec584
Saurabh Joshi <139209180+saurabh-joshi00@users.noreply.github.com>
2026-04-22 23:39:42 +0530

    react-hook-form integrated

--- src/components/Common/SelectBox.jsx
+++ src/components/Common/SelectBox.jsx
@@ -1,8 +1,8 @@
 import React, { useId } from 'react'
 
-function InputBox(ref, {
+function SelectBox(ref, {
+    options=[],
     label,
-    type='text',
     className='',
     ...props
 }) {
@@ -13,25 +13,25 @@ function InputBox(ref, {
     <>
       <div className='w-full'>
         {
-            label && <label 
-                    className='inline-block mb-1 pl-1' 
-                    htmlFor={id}
-                    >
-                        {label}
-                    </label>
+            label && <label htmlFor={id} className=''></label>
         }
 
-        <input 
+        <select
+            {...props}
             id={id}
             ref={ref}
-            type={type}
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            {...props}
-        />
+        >
+            {
+                options?.map((option) => (
+                    <option key={option} value={option}>{option}</option>
+                ))
+            }
+        </select>
 
       </div>
     </>
   )
 }
 
-export default InputBox
+export default SelectBox
--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,21 @@ import Logo from "./Common/Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import Button from "./Common/Button"
 import InputBox from "./Common/InputBox"
+import SelectBox from "./Common/SelectBox"
+import PostCard from "./Common/PostCard";
+import LoginPage from "./LoginPage/LoginPage";
+import SignupPage from "./SignupPage/SignupPage";
 
-export {Header, Footer, Container, Logo, LogoutBtn, Button, InputBox}
+export {
+    Header, 
+    Footer, 
+    Container, 
+    Logo, 
+    LogoutBtn, 
+    Button, 
+    InputBox, 
+    SelectBox, 
+    PostCard,
+    LoginPage,
+    SignupPage
+}
